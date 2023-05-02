e44ff1323a57d6fbb78c77a79208d38bf4d1b736
ElProfe <educaysoft@gmail.com>
2023-05-02 16:52:23 -0500

    mejoras

--- index.php
+++ index.php
@@ -33,7 +33,8 @@ document.getElementById("elnombre").innerHTML="El profesor";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/QuinonezBryan'>Quinonez Bryan</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+
  </script>
  
 
